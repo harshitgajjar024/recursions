881bee4a39fde2116dfc060c0394abc5c1128810
harshitgajjar024 <146190333+harshitgajjar024@users.noreply.github.com>
2024-10-03 10:57:35 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,19 @@
+//factorial using recusrion
+
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+    if (n<=1)
+        return 1;
+
+    else return (n*fact(n-1));
+}
+int main()
+{
+    int a;
+    cout<<"Enter an integer: "<<endl;
+    cin>>a;
+    cout<<"Factorial is: "<<fact(a);
+}
